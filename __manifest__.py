1c8931db3bf3b2d65f2fa59760feb6fc892e36c6
tanoggy <hoangductan@hotmail.com>
2019-05-27 21:56:50 +0700

    a

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    'name': "POS Oggy Custom",
+    'summary': """Custom CSS """,
+    'description': "",
+    'author': "",
+    'company': "",
+    'website': "",
+    'category': 'Point Of Sale',
+    'version': '0.1',
+    'depends': ['base', 'point_of_sale'],
+    'data': ['views/pos-oggy.xml'],
+    'qweb': [''],
+    'images': [''],
+    'license': '',
+    'installable': True,
+    'auto_install': False,
+}
